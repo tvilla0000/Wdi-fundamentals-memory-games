2ed99ee64639120cf2767e0a43810d02ea7d6ea3
tyler villa <bigboiii@tylers-MacBook-Pro.local>
2019-07-12 13:21:39 -0700

    created game logic

---
+++ memory_game/js/main.js
@@ -0,0 +1,9 @@
+console.log("Up and running!");
+
+var cardOne = "king";
+var cardTwo = "king";
+var cardThree = "Queen";
+var cardFour = "Queen";
+
+console.log("User flipped " + cardOne);
+console.log("User flipped " + cardThree);
